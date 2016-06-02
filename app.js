a24f9dd6032cad42c37c677448f5c10101a3ba45
Bryan Brophy <brybrophy@gmail.com>
2016-06-01 17:31:14 -0700

    Backup for update 7

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ $('.landing').click(function() {
 
 })
 
-$('.arrow').on('click', function() {
-  $('.arrow').addClass('.active');
-})
-
-
-var $xml = $.ajax({
+var $xhr = $.ajax({
   method: "GET",
   url: "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&gallery_id=72157666569424964&api_key=c1cc42b0a06a4e8d5b83d306ee92c0eb&format=json",
   dataType: "jsonp"
@@ -31,19 +26,35 @@ var $xml = $.ajax({
 
 
 var jsonFlickrApi = function(data) {
-  // console.log(data);
-
   var photos = data.photos.photo;
   var photo = photos[Math.floor(Math.random() * photos.length)];
   var id = photo.id;
   var serverId = photo.server;
   var farm = photo.farm;
   var secret = photo.secret;
-
   var galleryUrl = `https://farm${farm}.staticflickr.com/${serverId}/${id}_${secret}_b.jpg`;
 
   $('body').addClass('image');
-  $('.image').css('background-image', 'url(' + galleryUrl + ')')
+  $('.image').css('background-image', 'url(' + galleryUrl + ')');
+
+  $('.dot').off('click');
+
+  $('.dot').on('click', function() {
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+    jsonFlickrApi(data);
+    newPoem();
+    console.log(data);
+  });
+
+  $(document).off('keydown');
+  $(document).on('keydown', function(event) {
+    if (event.keyCode === 39) {
+      console.log('left');
+      jsonFlickrApi(data);
+      newPoem();
+    }
+  });
 }
 
 var url = 'https://cors-anywhere.herokuapp.com/poetrydb.org/author';
@@ -51,27 +62,33 @@ var url = 'https://cors-anywhere.herokuapp.com/poetrydb.org/author';
 var randomAuthor = function(data) {
   return Math.floor(Math.random() * 129);
 }
-var $xhl1 = $.getJSON(url, function(data) {
-  var authors = data.authors
 
-  var $xml2 = $.getJSON(`${url}/${authors[randomAuthor()]}`, function(data) {
+var getPoem = function() {
+  var $xhr1 = $.getJSON(url, function(data) {
+    var authors = data.authors
+    $('.poem').append(`<h4 class="load">loading poem...</h4>`);
+    var $xhr2 = $.getJSON(`${url}/${authors[randomAuthor()]}`, function(data) {
 
-    var $randomPoem = $xml2.done(function() {
-      var poem = data[Math.floor(Math.random() * data.length)];
-      var author = poem.author;
-      var title = poem.title;
-      var lines = poem.lines;
+      var $randomPoem = $xhr2.done(function() {
+        var poem = data[Math.floor(Math.random() * data.length)];
+        var author = poem.author;
+        var title = poem.title;
+        var lines = poem.lines;
+        $('.poem').empty();
+        $('.poem').append(`<h4>${title}</h4><h4>${author}</h4>`);
 
-      $('.poem').append(`<h4>${title}</h4><h4>${author}</h4>`);
-
-      lines.forEach(function(line) {
-        $('.poem').append(`<p>${line}</p>`);
+        lines.forEach(function(line) {
+          $('.poem').append(`<p>${line}</p>`);
+        })
       })
-
-    })
-
-
-
-
+    });
   });
-});
+};
+getPoem();
+
+var newPoem = function() {
+  $('.poem').empty();
+  $('.poem').append(`<h4 class="load">loading poem...</h4>`);
+  $('.load').animate({opacity: 1}, 1000);
+  getPoem();
+}
